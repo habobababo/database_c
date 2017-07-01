14edcaec6a900f4594d3c23a1d7e4142c0f5ad2e
habobababo <shadowman11221@gmail.com>
2017-07-01 17:12:38 +0200

    rework

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# database_c
+
+database_c
+===============
+
+****Requirements****<br>
+for database_c you need [mysqloo](https://facepunch.com/showthread.php?t=1515853)<br>
+
+
+# <i class="icon-file"></i> Developer documentation
+-------------
+<i class="icon-th-list">Serverside functions:
+>**corequery(querystr, callback)**
+**example:**<br>
+corequery("SELECT * FROM table WHERE id = '1' ", function(data)
+  PrintTable(data[1])
+end)
+
+----------
+powered by ![cc](http://37.228.134.43/files/files/logos/Logo/128x128_grey.png) [Core-Community.de](http://core-community.de/)
--- lua/autorun/server/database.lua
+++ lua/autorun/server/database.lua
@@ -1,57 +1,59 @@
+
 require( "mysqloo" )
 
-local DATABASE_HOST = "127.0.0.1"
-local DATABASE_PORT = 3306
-local DATABASE_NAME = ""
-local DATABASE_USERNAME = ""
-local DATABASE_PASSWORD = ""
+local C_HOST = "127.0.0.1"
+local C_PORT = 3306
+local C_DATABASE = ""
+local C_DATABASE_USERNAME = ""
+local C_DATABASE_USERPASSWORD = ""
 
-local function ConnectToDatabase()
+local function C_InitialConnect()
 
-	coredb = mysqloo.connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME, DATABASE_PORT)
-	
+	coredb = mysqloo.connect(C_HOST, C_PORT, C_DATABASE, C_DATABASE_USERNAME, C_DATABASE_USERPASSWORD)
+
 	function coredb:onConnected()
-		print("\n*** Connectet to Mysql Database ***") 
-		print("Host Info:", coredb:hostInfo() )
+		print("\n*** C_ Connected to the database! ***")
 		print("\n")
 	end
 
 	function coredb:onConnectionFailed( err )
-		print( "Connection to database failed!" )
+		print( "\n*** C_ Connection to database failed! ***" )
 		print( "Error:", err )
+		print("\n")
 	end
-	
+
 	coredb:connect()
-	
-	timer.Simple(1, function() hook.Call("DatabaseLoaded") end)
+
+	timer.Simple(1, function() hook.Call("C_DATABASE_LOADED") end)
 end
-hook.Add("Initialize", "Initialize_databse", ConnectToDatabase)
+hook.Add("Initialize", "C_InitialConnect", C_InitialConnect)
 
 function corequery(querystr, callback)
-	if !querystr then print("Querystr failed") return end
-	if !coredb then ConnectToDatabase(); timer.Simple(1.5, function() corequery(querystr, callback); end) end
+	if !querystr then return end
+	if !coredb then ConnectToDatabase(); timer.Simple(1.5, function() corequery(querystr, callback) end) end
 
 	local status = coredb:status()
 	if status == 2 or status == 3 then
-		print("Status Failed")
+		print("\n*** C_ Connection failed ***")
 		return
 	end
-	
+
 	local Query = coredb:query(querystr)
-	
-	if Query == nil then timer.Simple(1, function() corequery(querystr, callback); print("Query Failed... retrying") end) return end
-	
+
+	if Query == nil then timer.Simple(1, function() corequery(querystr, callback) end) return end
+
 	function Query.onSuccess( userdata )
 		if callback then
-			callback(Query:getData()) 
+			callback(Query:getData())
 		end
 	end
- 
+
     function Query:onError( err, sql )
-        print( "Query errored!" )
+        print( "\n*** C_ Query errored! ***" )
         print( "Query:", sql )
         print( "Error:", err )
+				print("\n")
     end
- 
+
     Query:start()
 end
